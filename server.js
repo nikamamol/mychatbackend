4a14114f56cccc61c160c1842cba9557fe377ad2
amol9145 <amol@koinetmedia.com>
2025-04-09 13:34:53 +0530

    NA

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const io = new Server(server, { cors: { origin: '*' } });
 
 // Middleware
 app.use(express.json());
-app.use(cors({ origin: 'https://privatechat-psi.vercel.app/' }));
+app.use(cors({ origin: '*' }));
 
 const mongoUri = "mongodb+srv://moreshital694:ocj0OSoXKXiRzRgP@cluster0.4aogt8i.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
